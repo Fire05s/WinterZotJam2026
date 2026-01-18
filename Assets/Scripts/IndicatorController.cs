5e6bd2f806d0933c75c65cadbd5cc521f781b390
Will Bao <willb@uci.edu>
2026-01-18 12:23:18 -0800

    indicator start

---
+++ Assets/Scripts/IndicatorController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class IndicatorController : MonoBehaviour
+{
+    [Header("Indicator")]
+    [SerializeField] private Animator _anim;
+
+    public void TriggerInspection()
+    {
+        
+    }
+
+    public void TriggerFlee()
+    {
+
+    }
+}
